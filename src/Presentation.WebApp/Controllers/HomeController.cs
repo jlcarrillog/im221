344c851481f9578ba8dfef818fa87c3f10c1895e
Luis Carrillo <luis@d3v.mx>
2021-10-02 16:27:02 -0500

    init

---
+++ src/Presentation.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
+using Presentation.WebApp.Models;
+using System.Diagnostics;
+
+using Domain;
+using Infrastructure;
+using Application;
+
+namespace Presentation.WebApp.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly CitasDbContext _citasDbContext;
+        public HomeController(IConfiguration configuration)
+        {
+            _citasDbContext = new CitasDbContext(configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public IActionResult Index()
+        {
+            // ToDo
+            return View("Error");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
